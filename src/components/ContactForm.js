bdec97e8e1bfa90b04b3957ea33a65f248dceeb5
Edema <edemaero@gmail.com>
2022-11-03 14:31:43 +0100

    style: contact page layouts updates

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -7,7 +7,7 @@ import TextAreaField from "./TextAreaField";
 const ContactForm = () => {
   const name = "BeyondLogic";
   return (
-    <div className="mt-[156px] xl:px-[360px] lg:px-[180px] md:px-[90px] px-[16px]">
+    <div className="md:mt-[156px] mt-[64px] xl:px-[360px] lg:px-[180px] md:px-[90px] px-[16px]">
       <div className="flex flex-col justify-center items-center">
         <div className="w-full">
           <h1 className="text-[36px] font-semibold leading-[44px] tracking-[-0.02em] text-[#101828]">
@@ -18,7 +18,7 @@ const ContactForm = () => {
           </p>
         </div>
         <div className="w-full">
-          <div className="flex space-x-[24px]">
+          <div className="flex md:flex-row flex-col md:space-x-[24px] md:space-y-0 space-y-[24px]">
             <InputField
               title="First name"
               type="text"
@@ -56,7 +56,7 @@ const ContactForm = () => {
           </div>
           <button
             id="btn__submit"
-            className="submit__button mt-[32px] mb-[230px] py-[12px] text-center bg-[#1570EF] border border-[#1570EF] rounded-[8px] w-full text-white"
+            className="submit__button mt-[32px] md:mb-[236px] mb-[146px] py-[12px] text-center bg-[#1570EF] border border-[#1570EF] rounded-[8px] w-full text-white"
           >
             Send message
           </button>
