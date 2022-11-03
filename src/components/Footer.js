3dfdd5241d36d5cde2f4da70d64424247e12d618
Edema <edemaero@gmail.com>
2022-11-03 23:06:45 +0100

    style: footer layout updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,22 @@ import React from "react";
 
 import ZuriInternshipLogo from "../assets/ZuriInternshipLogo.svg";
 import I4Glogo from "../assets/I4GLogo.svg";
-import "../"
+import "../";
 
 const Footer = () => {
   return (
-    <div className="border-t border-[#EAECF0] flex md:flex-row flex-col md:justify-between justify-center md:items-start items-center pt-[42px] pb-[40px]  md:space-y-0 space-y-4">
+    <div className="border-t border-[#EAECF0] flex md:flex-row flex-col md:justify-between justify-start items-start pt-[42px] md:pb-[40px] pb-[32px]  md:space-y-0 space-y-[16.21px]">
       <img
         src={ZuriInternshipLogo}
         alt="Zuri Internship logo"
-        className="lg:w-auto xs:w-1/5 w-1/3"
+        className="lg:w-auto  w-[138.77px]"
       />
-      <h4 className="font-normal text-[16px] text-[#667085] leading-[24px]">
+      <h4 className="font-normal md:text-[16px] text-[14px] text-[#667085] md:leading-[24px] leading-[20px]">
         HNG Internship 9 Frontend Task
       </h4>
-      <img src={I4Glogo} alt="I4G logo" className="lg:w-auto xs:w-1/5 w-1/3" />
+      <img src={I4Glogo} alt="I4G logo" className="lg:w-auto w-[99.1px]" />
     </div>
   );
 };
 
 export default Footer;
-
